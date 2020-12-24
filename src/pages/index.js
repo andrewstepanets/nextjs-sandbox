c81a0df84e8317afd8f41a30092c84e530567eb6
Andrii Stepanets <ovrass@gmail.com>
2020-12-24 16:03:15 +0200

    Added styling Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Layout from './components/Layout'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <Head>
         <title>This is My First Next JS App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-3/4 flex flex-col justify-center">
-        <img src="/assets/img_logo.svg" alt="Andrew Steps Logo" className="m-auto my-4 w-24 sm:w-auto" />
+      <main className="bg-white p-4">
+        <Hero />
+        {/* <img src="/assets/img_logo.svg" alt="Andrew Steps Logo" className="m-auto my-4 w-24 sm:w-auto" />
         <h1 className="mx-auto text-4xl font-bold">
           Welcome to Next.js
         </h1>
         <Link href="/page-two">
           <a className="text-red-400">Go Page Two</a>
-        </Link>
+        </Link> */}
       </main>
 
-    </div>
+    </Layout>
   )
 }
