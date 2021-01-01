e3400026cad66e8afdcf745f5a7791b3cf6dd903
Andrii Stepanets <ovrass@gmail.com>
2021-01-01 20:26:10 +0200

    Purged CSS and moved components folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Layout from './components/Layout'
-import Hero from './components/Hero'
+// import Link from 'next/link'
+import Layout from '../components/Layout'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
